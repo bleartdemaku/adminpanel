c98176eea406711f120a7f245a206b7762617d49
bleartdemaku <bd47165@ubt-uni.net>
2023-04-02 19:04:00 +0200

    added chart component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,18 @@
 import Chart from "../../components/chart/Chart";
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
+import { userData } from "../../dummyData";
 import "./home.css";
 
 const Home = () => {
   return (
     <div className="home">
       <FeaturedInfo />
-      <Chart />
+      <Chart
+        data={userData}
+        title="User Analytics"
+        grid
+        dataKey="Active User"
+      />
     </div>
   );
 };
