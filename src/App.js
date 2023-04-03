b864bccdd1139ac6bdfd94225e4330f8bd717c4d
bleartdemaku <bd47165@ubt-uni.net>
2023-04-03 21:46:29 +0200

    added user list page

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import "./App.css";
 import Home from "./pages/home/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import UserList from "./pages/userList/UserList";
 
 function App() {
   return (
-    <>
+    <Router>
       <Topbar />
       <div className="container">
         <Sidebar />
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/users" element={<UserList />}></Route>
+        </Routes>
       </div>
-    </>
+    </Router>
   );
 }
 
--- src/dummyData.js
+++ src/dummyData.js
@@ -48,3 +48,96 @@ export const userData = [
     "Active User": 4490,
   },
 ];
+
+export const userRows = [
+  {
+    id: 1,
+    username: "Jon Snow",
+    avatar:
+      "https://img.freepik.com/free-photo/young-japanese-woman-with-jacket_23-2148870740.jpg?w=740&t=st=1680549911~exp=1680550511~hmac=0eb6497d6304ada1d2ff5d39ea6d72762a68274759c43882a28cc51d240d5087",
+    email: "jon@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  {
+    id: 2,
+    username: "Jon Snow",
+    avatar:
+      "https://e1.pxfuel.com/desktop-wallpaper/534/172/desktop-wallpaper-stylish-people-to-follow-on-instagram-instagram-girl-profile-pic.jpg",
+    email: "jon@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  {
+    id: 3,
+    username: "Jon Snow",
+    avatar:
+      "https://img.freepik.com/free-photo/young-japanese-woman-with-jacket_23-2148870740.jpg?w=740&t=st=1680549911~exp=1680550511~hmac=0eb6497d6304ada1d2ff5d39ea6d72762a68274759c43882a28cc51d240d5087",
+    email: "jon@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  {
+    id: 4,
+    username: "Jon Snow",
+    avatar:
+      "https://e1.pxfuel.com/desktop-wallpaper/534/172/desktop-wallpaper-stylish-people-to-follow-on-instagram-instagram-girl-profile-pic.jpg",
+    email: "jon@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  {
+    id: 5,
+    username: "Jon Snow",
+    avatar:
+      "https://img.freepik.com/free-photo/young-japanese-woman-with-jacket_23-2148870740.jpg?w=740&t=st=1680549911~exp=1680550511~hmac=0eb6497d6304ada1d2ff5d39ea6d72762a68274759c43882a28cc51d240d5087",
+    email: "jon@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  {
+    id: 6,
+    username: "Jon Snow",
+    avatar:
+      "https://e1.pxfuel.com/desktop-wallpaper/534/172/desktop-wallpaper-stylish-people-to-follow-on-instagram-instagram-girl-profile-pic.jpg",
+    email: "jon@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  {
+    id: 7,
+    username: "Jon Snow",
+    avatar:
+      "https://img.freepik.com/free-photo/young-japanese-woman-with-jacket_23-2148870740.jpg?w=740&t=st=1680549911~exp=1680550511~hmac=0eb6497d6304ada1d2ff5d39ea6d72762a68274759c43882a28cc51d240d5087",
+    email: "jon@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  {
+    id: 8,
+    username: "Jon Snow",
+    avatar:
+      "https://e1.pxfuel.com/desktop-wallpaper/534/172/desktop-wallpaper-stylish-people-to-follow-on-instagram-instagram-girl-profile-pic.jpg",
+    email: "jon@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  {
+    id: 9,
+    username: "Jon Snow",
+    avatar:
+      "https://img.freepik.com/free-photo/young-japanese-woman-with-jacket_23-2148870740.jpg?w=740&t=st=1680549911~exp=1680550511~hmac=0eb6497d6304ada1d2ff5d39ea6d72762a68274759c43882a28cc51d240d5087",
+    email: "jon@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  {
+    id: 10,
+    username: "Jon Snow",
+    avatar:
+      "https://e1.pxfuel.com/desktop-wallpaper/534/172/desktop-wallpaper-stylish-people-to-follow-on-instagram-instagram-girl-profile-pic.jpg",
+    email: "jon@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+];
