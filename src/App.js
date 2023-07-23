63bc692bbae1bb95ede0c6209042d4c4cd989e50
bleartdemaku <bd47165@ubt-uni.net>
2023-07-23 16:17:39 +0200

    fetching data from db chart data

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import NewUser from "./pages/newUser/NewUser";
 import ProductList from "./pages/productList/ProductList";
 import Product from "./pages/product/Product";
 import NewProduct from "./pages/newProduct/NewProduct";
+import { useEffect, useMemo, useState } from "react";
+import axios from "axios";
 
 function App() {
   return (
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,20 +1,61 @@
+import { useEffect, useMemo, useState } from "react";
 import Chart from "../../components/chart/Chart";
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
 import WidgetLg from "../../components/widgetLg/WidgetLg";
 import WidgetSm from "../../components/widgetSm/WidgetSm";
 import { userData } from "../../dummyData";
 import "./home.css";
+import axios from "axios";
 
 const Home = () => {
+  const MONTHS = useMemo(
+    () => [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Agu",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+    []
+  );
+
+  const [userStats, setUserStats] = useState([]);
+
+  useEffect(() => {
+    const getStats = async () => {
+      try {
+        const res = await axios.get("/users/stats", {
+          headers: {
+            token:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ODA4OTBmYTdiMGE4YmY0MDM1ZjdlMSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY4NjIyMDM4MywiZXhwIjoxNjg2NjUyMzgzfQ.f8yxbdjRoiBV4-hfL1mpHghACc45c-vPYyVMILyiuuQ",
+          },
+        });
+        const statsList = res.data.sort(function (a, b) {
+          return a._id - b._id;
+        });
+        statsList.map((item) =>
+          setUserStats((prev) => [
+            ...prev,
+            { name: MONTHS[item._id - 1], "New User": item.total },
+          ])
+        );
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getStats();
+  }, [MONTHS]);
   return (
     <div className="home">
       <FeaturedInfo />
-      <Chart
-        data={userData}
-        title="User Analytics"
-        grid
-        dataKey="Active User"
-      />
+      <Chart data={userStats} title="User Analytics" grid dataKey="New User" />
       <div className="homeWidgets">
         <WidgetSm />
         <WidgetLg />
